7011f7929e009d9787505f617d0ba96f85ea423e
Hidari <ika5140@gmail.com>
2025-01-05 03:48:40 +0900

    feat: update rip to v0.2.0

--- Formula/rip.rb
+++ Formula/rip.rb
@@ -1,14 +1,14 @@
 class Rip < Formula
     desc "Handling cross-platform ZIP archives that just work everywhere"
     homepage "https://github.com/hidari/rip-zip"
-    version "0.1.2"  # あなたのプロジェクトの現在のバージョン
+    version "0.2.0"  # あなたのプロジェクトの現在のバージョン
 
     if OS.mac? && Hardware::CPU.intel?
       url "https://github.com/hidari/rip-zip/releases/download/v#{version}/rip-zip-x86_64-apple-darwin.zip"
-      sha256 "8a9a867b65e08894d38776daf255cd798413b52844f686475e4c8d383bdb4a2c" # バイナリのSHA256チェックサム
+      sha256 "21c36d0f607c130f70ebfce0baf6dc14bea63a955ab3f7319523e1fe267530e4" # バイナリのSHA256チェックサム
     elsif OS.mac? && Hardware::CPU.arm?
       url "https://github.com/hidari/rip-zip/releases/download/v#{version}/rip-zip-aarch64-apple-darwin.zip"
-      sha256 "f7a79b8bb8f85adaf95f51b656aa7a51ef513f85fd8988519de027930c292ac2" # バイナリのSHA256チェックサム
+      sha256 "d0ca74e89f0f026d523016ff662099f41a52ff30b439d7c01d5268efc963ecb3" # バイナリのSHA256チェックサム
     end
 
     def install
